f45267f0217324839e42f58bdbc54ebe0c191e21
dst <dst@mini-pc-ubuntu>
2026-06-10 03:53:24 +1000

    Real-life extraction 1: ExtractResult

---
+++ src/extractresult.rs
@@ -0,0 +1,9 @@
+use crate::spans::{merge_spans, ByteSpan};
+#[derive(Debug)]
+pub struct ExtractResult {
+    pub new_file_path: String,
+    pub test_file_path: Option<String>,
+    pub items_extracted: Vec<String>,
+    pub usage_files_updated: Vec<String>,
+    pub extracted_spans: Vec<ByteSpan>,
+}
